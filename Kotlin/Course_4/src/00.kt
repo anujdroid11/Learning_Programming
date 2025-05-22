5219a2538cfe9a6b8d386485807fca5bc9f755c6
Anuj Singh <anujdroid11@gmail.com>
2025-05-23 03:28:34 +0530

    kotlin learning

--- Kotlin/Course_4/src/00.kt
+++ Kotlin/Course_4/src/00.kt
@@ -1,4 +1,31 @@
 fun main() {
-    
+
+    var number1 = 25
+    var number2: Int = 23
+
+    println("Hello World")
+    println("This is second line ")
+    println("the number is $number1")
+    println("the 2nd number is $number2")
+
+    var number3: Float = 67.22F
+    println("The float number is $number3")
+
+    var number4: Double = 258.65
+    println("The double number is $number4")
+
+    var text1: Char = 'A'
+    var text2: String = "This is string datatype 456 "
+
+    println("This is $text1")
+    println("String is $text2")
+
+    var bool: Boolean = true
+    var bool2: Boolean = false
+
+    println(bool)
+    println(bool2)
+
+
 
 }
